748f8a30ced1ebd1caf6b263ddafe000c76bc1ed
taylormarcos530-ship-it <taylormarcos530@gmail.com>
2026-09-08 23:41:07 -0300

    feat: connect permanent Android player preview APK

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.VISION_CONFIG = Object.freeze({
   supabaseUrl: 'https://fpadgedrgcxrrqflzhjt.supabase.co',
   supabasePublishableKey: 'sb_publishable_pSGsns390QFjOY4JHs-Xqw_bGrX_kCN',
   storageBucket: 'vision-media',
-  androidPlayerApkUrl: ''
+  androidPlayerApkUrl: './downloads/Vision-Player-preview.apk'
 });
